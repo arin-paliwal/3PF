a9b019f07435c9a37600b7b89f724044c1e5053e
Arin Paliwal <paliwalarin09@gmail.com>
2023-03-26 02:10:11 +0530

    Added Experience & About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import Tilt from 'react-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
+import { SectionWrapper } from '../hoc';
 import {fadeIn,textVariant} from "../utils/motion";
 
 const ServiceCard=({index,title,icon})=>{
@@ -18,6 +19,7 @@ const ServiceCard=({index,title,icon})=>{
         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
           <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
         </div>
       </motion.div>
       {title}
@@ -60,4 +62,4 @@ const About = () => {
   );
 }
 
-export default About
+export default SectionWrapper(About,"about")
