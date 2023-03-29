b2e608c00b804ac8eb2fb8d986961233022d40e4
Arin Paliwal <paliwalarin09@gmail.com>
2023-03-30 02:58:02 +0530

    .

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import
+import Tilt from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants'
+import { fadeIn,textVariant } from '../utils/motion'
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.heroHeadText}>Projects.</h2>
+      </motion.div>
+      <div className='w-full flex'>
+        <motion.p 
+        variants={fadeIn("","",0.1,1)}
+        className='mt-3 text-secondary max-w-[3xl] text-[17px] leading-[30px]'
+        >
+          
+        </motion.p>
+      </div>
+    </>
+  );
 }
 
-export default Works
+export default SectionWrapper(Works,"");
