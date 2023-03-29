d2f37bf79187c5eaf676ca066e3c046b392623a6
Arin Paliwal <paliwalarin09@gmail.com>
2023-03-30 03:15:59 +0530

    Added Project Cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,7 +6,28 @@ import { github } from '../assets'
 import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn,textVariant } from '../utils/motion'
-
+const ProjectCard=({index,name,description,tags,image,source_code_link})=>{
+  return(
+    <motion.div variants={fadeIn("up","spring",index*0.5,0.75)}>
+      <Tilt options={{
+        max:45,
+        scale:1,
+        speed:450
+      }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className='relative w-full h-[230px]'>
+          <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' />
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div onClick={()=>window.open (source_code_link,"_blank")} className='black-gradient w-10 h-10 rounded-full flex justify-center cursor-pointer items-center'>
+              <img src={github} alt={github} className="w-1/2 h-1/2 object-contain"/>
+            </div>
+          </div>
+        </div>
+      </Tilt>
+    </motion.div>
+  )
+}
 const Works = () => {
   return (
     <>
@@ -14,13 +35,32 @@ const Works = () => {
         <p className={styles.sectionSubText}>My Work</p>
         <h2 className={styles.heroHeadText}>Projects.</h2>
       </motion.div>
-      <div className='w-full flex'>
-        <motion.p 
-        variants={fadeIn("","",0.1,1)}
-        className='mt-3 text-secondary max-w-[3xl] text-[17px] leading-[30px]'
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary max-w-[3xl] text-[17px] leading-[30px]"
         >
-          
+          As I showcase my projects, I am excited to share the culmination of my
+          hard work and dedication to the field of technology. Through these
+          projects, I have honed my skills in programming, design, and project
+          management, and have pushed the boundaries of what is possible in this
+          ever-evolving field. Each project represents a unique challenge and an
+          opportunity to innovate, and I am proud of the solutions I have
+          created to solve real-world problems. I am eager to demonstrate how my
+          projects have improved user experience, increased efficiency, and
+          provided valuable insights to businesses and organizations. As I
+          present my projects, I hope to inspire others to pursue their own
+          ideas and push the boundaries of what is possible with technology.
         </motion.p>
+      </div>
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project,index)=>(
+          <ProjectCard
+          key={`project-${index}`}
+          index={index}
+          {...project}
+          />
+        ))}
       </div>
     </>
   );
