a8812179423ff64f2ba89531e50ed059d6dc7fec
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-02 15:20:17 +0530

    Fixed Mobile Hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,29 @@ import { logo,menu,close } from "../assets";
 const Navbar = () => {
   const [active, setActive] = useState('');
   const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <nav
-      className={`&{styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
+        scrolled ? "bg-primary" : "bg-transparent"
+      }`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -42,14 +62,24 @@ const Navbar = () => {
         </ul>
 
         {/* for mobile navigation bar */}
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div
+          className={`${
+            scrolled ? "bg-primary" : "bg-transparent"
+          } sm:hidden flex flex-1 justify-end items-center`}
+        >
           <img
             src={toggle ? close : menu}
             alt="menu"
             className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
-          <div className="{`${!toggle?'hidden':'flex'}p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}">
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl   ${
+              scrolled ? "bg-primary" : "bg-transparent"
+            }  `}
+          >
             <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
               {navLinks.map((nav) => (
                 <li
