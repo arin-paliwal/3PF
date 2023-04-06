b6063afadcb40ab256d2adcab30ef56200037e91
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-07 03:12:12 +0530

    Added Custom Mouse Poiner

---
+++ pointer.js
@@ -0,0 +1,17 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+window.addEventListener("mousemove",function(e){
+    const positionX=e.clientX;
+    const positionY=e.clientY;
+    cursorDot.style.left=`${positionX}px`;
+    cursorDot.style.top = `${positionY}px`;
+
+    cursorOutline.animate({
+        left:`${positionX}px`,
+        top:`${positionY}px`
+    },
+    {
+        duration:800,fill:"forwards"
+    }
+    )
+})
