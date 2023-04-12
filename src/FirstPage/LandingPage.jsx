aad13b196f59de1572f90c2109b5ac3de91fdc7c
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-12 13:57:44 +0530

    .

--- src/FirstPage/LandingPage.jsx
+++ src/FirstPage/LandingPage.jsx
@@ -138,8 +138,19 @@ const Introduction = () => {
     <div>
       <Particles options={particlesConfigState} init={init} />
       <div className="relative z-[-1] bg-cover bg-no-repeat bg-center">
-        <Navbar />
-        <About />
+        {/* <Navbar /> */}
+        {/* <About /> */}
+      </div>
+      <div>
+        <button>Arin</button>
+      </div>
+      <div className="flex justify-center xs:mt-10 md:mt-0">
+        <button className="btn-43 mt-10">
+          <Link to="/about">
+            <span className="old">Know More</span>
+            <span className="new">About Me!</span>
+          </Link>
+        </button>
       </div>
     </div>
   );
