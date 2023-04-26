dd2bb73170783efc04f66dedf06a961349b4da49
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-26 14:16:20 +0530

    added icons

--- src/FirstPage/Socialicons.jsx
+++ src/FirstPage/Socialicons.jsx
@@ -5,34 +5,44 @@ const Socialicons = () => {
     <div className="icons">
       <div className="wrapper">
         <div className="button">
-          <div className="icon">
-            <i className="fab fa-facebook-f"></i>
-          </div>
-          <span>Facebook</span>
+          <a href="https://www.facebook.com/paliwal.arin" target="_blank">
+            <div className="icon">
+              <i className="fab fa-facebook-f"></i>
+            </div>
+            <span>Facebook</span>
+          </a>
         </div>
         <div className="button">
-          <div className="icon">
-            <i className="fab fa-twitter"></i>
-          </div>
-          <span>Twitter</span>
+          <a href="https://twitter.com/arin_paliwal" target="_blank">
+            <div className="icon">
+              <i className="fab fa-twitter"></i>
+            </div>
+            <span>Twitter</span>
+          </a>
         </div>
         <div className="button">
-          <div className="icon">
-            <i className="fab fa-instagram"></i>
-          </div>
-          <span>Instagram</span>
+          <a href="https://www.instagram.com/arin.paliwal/" target="_blank">
+            <div className="icon">
+              <i className="fab fa-instagram"></i>
+            </div>
+            <span>Instagram</span>
+          </a>
         </div>
         <div className="button">
-          <div className="icon">
-            <i className="fab fa-github"></i>
-          </div>
-          <span>Github</span>
+          <a href="https://github.com/arin-paliwal" target="_blank">
+            <div className="icon">
+              <i className="fab fa-github"></i>
+            </div>
+            <span>Github</span>
+          </a>
         </div>
         <div className="button">
-          <div className="icon">
-            <i className="fab fa-youtube"></i>
-          </div>
-          <span>YouTube</span>
+          <a href="https://www.linkedin.com/in/arinpaliwal/" target="_blank">
+            <div className="icon">
+              <i className="fab fa-linkedin"></i>
+            </div>
+            <span>LinkedIn</span>
+          </a>
         </div>
       </div>
     </div>
