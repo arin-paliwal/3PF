42120d2cbf595339a78379c0d1de8bef018e5940
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-28 10:40:17 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,13 +65,13 @@ const Navbar = () => {
           <img
             src={toggle ? close : menu}
             alt="menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            className="w-[35px] h-[35px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-20 absolute top-20 right-0 mx-3 my-2 min-w-[140px] z-10 rounded-xl bg-primary`}
+            } p-20 absolute top-20 right-0 mx-3 my-2 min-w-[140px] z-10 rounded-3xl bg-primary`}
           >
             <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
               {navLinks.map((nav) => (
