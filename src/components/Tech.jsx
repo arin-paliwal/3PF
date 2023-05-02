0a0f6130aeb9870622b2d58be6821621ba4ef394
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-02 16:50:01 +0530

    Added Skills Cards

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { BallCanvas } from './canvas'
-import { SectionWrapper } from '../hoc'
-import { technologies } from '../constants'
-import { styles } from '../styles'
-import { motion } from 'framer-motion'
-import { fadeIn } from '../utils/motion'
+import React from "react";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import { styles } from "../styles";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
+import "./skillsMobile.css";
 
 const Tech = () => {
   return (
@@ -20,6 +21,63 @@ const Tech = () => {
           </div>
         ))}
       </div>
+
+      {/* <div className="cardsSkills">
+        <section class="main">
+          <div class="wrap wrap--1">
+            <div class="container container--1">
+              <p>Java</p>
+            </div>
+          </div>
+
+          <div class="wrap wrap--2">
+            <div class="container container--2">
+              <p>Python</p>
+            </div>
+          </div>
+
+          <div class="wrap wrap--3">
+            <div class="container container--3">
+              <p>React</p>
+            </div>
+          </div>
+          <div class="wrap wrap--3">
+            <div class="container container--3">
+              <p>Git</p>
+            </div>
+          </div>
+          <div class="wrap wrap--3">
+            <div class="container container--3">
+              <p>MongoDB</p>
+            </div>
+          </div>
+          <div class="wrap wrap--3">
+            <div class="container container--3">
+              <p>C / C++</p>
+            </div>
+          </div>
+          <div class="wrap wrap--3">
+            <div class="container container--3">
+              <p>JavaScript</p>
+            </div>
+          </div>
+          <div class="wrap wrap--3">
+            <div class="container container--3">
+              <p>Machine Learning</p>
+            </div>
+          </div>
+          <div class="wrap wrap--3">
+            <div class="container container--3">
+              <p>React</p>
+            </div>
+          </div>
+          <div class="wrap wrap--3">
+            <div class="container container--3">
+              <p>Figma</p>
+            </div>
+          </div>
+        </section>
+      </div> */}
       <div className="portfolio-experiment flex justify-center mt-8 p-6">
         <a href="/projects">
           <span className="text">My Projects !</span>
@@ -31,6 +89,6 @@ const Tech = () => {
       </div>
     </>
   );
-}
+};
 
 export default SectionWrapper(Tech, "technologies");
