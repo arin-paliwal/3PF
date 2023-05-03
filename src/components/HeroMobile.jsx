8bdc5f7c2d429e10250791d148593ec96e7028f8
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-03 14:36:05 +0530

    Build No.8

--- src/components/HeroMobile.jsx
+++ src/components/HeroMobile.jsx
@@ -27,22 +27,8 @@
 import React, { useState, useEffect } from "react";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
-import "./typo.css"
 
 const HeroMobile = () => {
-  const [index, setIndex] = useState(0);
-  const texts = ["Bonjour", "Hello", "Namaste", "Hola", "Salve", "Konnichiwa"]; // an array of texts to display 
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIndex((index + 1) % texts.length);
-    }, 3000); // set the interval time for 3 seconds
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [index, texts.length]);
-
   return (
     <div className="mt-0">
       <div
@@ -52,18 +38,6 @@ const HeroMobile = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#e37926]">Arin Paliwal</span>
             <br />
-            <div className="flex">
-              <div className="text-[#6b6a69] mt-10 mb-10">
-                {texts.map((text, i) => (
-                  <span
-                    key={i}
-                    className={i === index ? "fade-in" : "fade-out"}
-                  >
-                    {text}
-                  </span>
-                ))}
-              </div>
-            </div>
           </h1>
         </div>
       </div>
