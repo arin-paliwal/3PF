859ae7d851bfac141c424cafd22656f9b44c5a4b
Arin Paliwal <paliwalarin09@gmail.com>
2023-06-29 00:06:24 +0530

    ..

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Experience from './Experience';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
-import AboutHero from './Abouthero';
+import AboutHero from './AboutHero';
 
 export {
   Hero,
